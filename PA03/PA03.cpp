2ce0c326e3d18ea46caf85e0b1a611dc15bc0bd5
Grant <gbaumstark@micds.org>
2026-08-18 23:05:07 -0500

    forgot to save lol

--- PA03/PA03.cpp
+++ PA03/PA03.cpp
@@ -4,34 +4,21 @@
 #include <string>
 #include <vector>
 
+//make a struct for each person so that printing is easier and then i can make a tostring method??
+//is this the best way to do it? Prob not but it seems intuative
+struct student {
+    std::string name;
+    std::string letterGrade;
+    int grade;
+};
 
-//should only accept two args, input file and output file
-int main(int argc, char* argv[]){
-
-    //make a struct for each person so that printing is easier and then i can make a tostring method??
-    //is this the best way to do it? Prob not but it seems intuative
-    struct student {
-        std::string name;
-        std::string letterGrade;
-        int grade;
-    };
-
-    //storing the input and output file names
-    std::string inputFile = argv[1];
-    std::string outputFile = argv[2];
-
-    //some helper methods that i am keeping within the main class cause scope
-    int readData(ifstream inFile(inputFile)){ //why the fuck is this upset fix this
-        //do when home
-    }
-    int writeData(ofstream outFile(outputFile))){ //and of course this shit is fine and the other one isnt even tho its the fucking same
-        //do when home
-    }
-
+//some helper methods 
+int readData(std::ifstream& inFile){
+    //do when home
+}
+int writeData(std::ofstream& outFile){ 
 }
 
-
-//and some more methods that do not need to be in scope
 std::string calculateGrade(int grade) {
     if(grade >= 90){
         return "A"; //grade is an a
@@ -52,3 +39,18 @@ std::string calculateGrade(int grade) {
     int highestScore(){
         //need to do this
     }
+
+
+//should only accept two args, input file and output file
+int main(int argc, char* argv[]){
+
+    //storing the input and output file names
+    std::string inputFile = argv[1];
+    std::string outputFile = argv[2];
+
+    //starting the input file streams
+    std::ifstream inFile(inputFile);
+    std::ofstream outFile(outputFile);
+
+}
+
