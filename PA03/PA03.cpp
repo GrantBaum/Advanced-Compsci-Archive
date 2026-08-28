e6563d086ac4bc385a8374765644c1978c579aac
Grant <gbaumstark@micds.org>
2026-08-28 12:38:24 -0500

    this works not

--- PA03/PA03.cpp
+++ PA03/PA03.cpp
@@ -3,11 +3,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <exception> //needed this for the try/catch
 
 //make a struct for each person so that printing is easier and then i can make a tostring method??
 //is this the best way to do it? Prob not but it seems intuative
 struct student {
-
     //struct instance vars
     std::string name;
     std::string letterGrade;
@@ -16,10 +16,8 @@ struct student {
 
 //vector of students
 std::vector<student> studentsVec;
-
 //some helper methods
 std::string calculateGrade(int grade) {
-
     if(grade >= 90){
         return "A"; //grade is an a
     }
@@ -37,94 +35,105 @@ std::string calculateGrade(int grade) {
     }
 }
 
-int readData(std::ifstream& inFile){
-
+void readData(std::ifstream& inFile){
     //needed vars
-    int num;
-    std::string name;
+    std::string line;
 
-    while(inFile >> name >> num){ //i need to make sure this will parse with whitespace in between names
+    //getline takes the WHOLE line so the spaces come along for the ride
+    while(std::getline(inFile, line)){
 
-        //will grab the next number grade down the line
-        //i could then make a vector of the people struct and go down the line initializing names and grades
+        //if theres nothing but whitespace on this line just move on
+        if(line.find_first_not_of(" \t\r") == std::string::npos){
+            continue;
+        }
+
+        size_t end = line.find_last_not_of(" \t\r");
+        line = line.substr(0, end + 1);
+        
+        //the name is the part thats a random length, the number is always last
+        //so i split from the END instead of the front. this is the whole trick
+        size_t split = line.find_last_of(" \t");
+        if(split == std::string::npos){
+            //no space at all means this line is junk
+            std::cerr << "skipping malformed line: " << line << std::endl;
+            continue;
+        }
+        std::string name = line.substr(0, split); //everything before the last space
+        std::string gradeText = line.substr(split + 1); //everything after
+        //stoi throws instead of failing quiet
+        int num;
+        try {
+            num = std::stoi(gradeText);
+        }
+        catch(const std::exception&) {
+            std::cerr << "skipping malformed line: " << line << std::endl;
+            continue;
+        }
+        //same as before, build the student and shove it in the vector
         student s;
         s.name = name;
         s.grade = num;
         s.letterGrade = calculateGrade(num);
-
-        //this gets the initialized student and creates the vector. SO after reading the file, it is now all in the vector
         studentsVec.push_back(s);
     }
-
-    return 0;
-
 }
-
-int writeData(std::ofstream& outFile){ 
-
-    for(student s: studentsVec){
-        //needed tostring to concatonate string and int values. Noted.
-        outFile << s.name + " " + std::to_string(s.grade) + " " + s.letterGrade << std::endl;
+void writeData(std::ofstream& outFile){
+    //const student& instead of student so it stops copying the whole struct every loop
+    for(const student& s: studentsVec){
+        //thought i needed a tostring operator but this works too
+        outFile << s.name << " " << s.grade << " " << s.letterGrade << std::endl;
     }
-
-    return 0;
 }
-
 int highestScore(){
        int temp = 0;
-
-       for(student s : studentsVec) {
-        //if the most recent student score is higher, then they replace the temp
+       for(const student& s : studentsVec) {
             if(s.grade > temp){
                 temp = s.grade;
             }
        }
-
        //once loop terminates return highest score
        return temp;
 }
-
-
 //should only accept two args, input file and output file
 int main(int argc, char* argv[]){
-
-    //storing the input and output file names
-    std::string inputFile = argv[1];
-    std::string outputFile = argv[2];
-
-    //starting the input file streams
-    std::ifstream inFile(inputFile);
-    std::ofstream outFile(outputFile);
-
     if(argc != 3){
         //this means invalid number of args
         std::cerr << "Invalid number of arguments" << std::endl;
         return 1;
     }
-    else{
-        
-        //we good lets start doing some stuff
-        if(inFile) {
-            //read data
-            readData(inFile); //this is simple
-            //write data
-            if(outFile){
-                writeData(outFile); //this is easy
-            }
-            else {
-                //couldnt open output file
-                std::cerr << "failed to open output file" << std::endl;
-                return 1;
-            }
-            //print highest score
-            std::cout << highestScore() << std::endl;
-        }
-        else {
-            //couldnt open input file
-            std::cerr << "failed to open input file" << std::endl;
-            return 1;
-        }
+    //now its safe to touch these
+    std::string inputFile = argv[1];
+    std::string outputFile = argv[2];
+
+    std::ifstream inFile(inputFile);
+    if(!inFile) {
+        //couldnt open input file
+        std::cerr << "failed to open input file" << std::endl;
+        return 1;
     }
 
-}
+    //check the output file up here too. no point parsing the whole thing
+    //and THEN finding out i cant write anywhere
+    std::ofstream outFile(outputFile);
+    if(!outFile) {
+        //couldnt open output file
+        std::cerr << "failed to open output file" << std::endl;
+        return 1;
+    }
 
+    //we good lets start doing some stuff
+    //read data
+    readData(inFile); //this is simple
+
+    //if every single line was junk theres nothing to write
+    if(studentsVec.empty()){
+        std::cerr << "no valid records found in input file" << std::endl;
+        return 1;
+    }
+
+    //write data
+    writeData(outFile); //this is easy
+    //print highest score
+    std::cout << highestScore() << std::endl;
+    return 0;
+}
