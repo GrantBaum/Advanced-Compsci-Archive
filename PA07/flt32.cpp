879c062a3aae0e0709bfcbeedc263b13700d0cee
Grant <gbaumstark@micds.org>
2026-09-22 12:41:04 -0500

    working get sign, exp, val, and all. Tested.

--- PA07/flt32.cpp
+++ PA07/flt32.cpp
@@ -2,13 +2,15 @@
 #include "flt32.h"
 
 int flt32_get_sign (flt32 x) {
-  return 0; /* implement this */
+  return getBit(x, 31); //returns most significant bit
 }
 int flt32_get_exp (flt32 x) {
-  return 0; /* implement this */
+  return getField(x, 23, 30, 0); //exponent bits, unsigned
 }
 int flt32_get_val (flt32 x) {
-  return 0;
+  unsigned int newV = static_cast<unsigned int>(getField(x, 0, 22, 0)); //0-22 unsigned
+  newV = setBit(newV, 23); //setting implied one
+  return newV;
 }
 void flt32_get_all(flt32 x, int* sign, int*exp, int* val) {
   *sign = flt32_get_sign(x);
